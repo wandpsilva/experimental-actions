05243900d3c9567a53632215b9288443c3ceedc3
wander <wanderdesigner@gmail.com>
2023-05-23 19:33:40 -0300

    trying

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ n2=${INPUT_N2}
 
 if [ $n1 -eq 0 ] || [ $n2 -eq 0 ]; then
       echo "informe valores maiores que zeros!"
+elif
+      echo "resultado da soma: $(expr $n1 + $n2)"
 fi
-
-echo "$n1 and $n2"
