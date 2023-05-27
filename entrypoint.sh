a675c94bd2d1c396554aad48e55475aa1ce5c1aa
wander <wanderdesigner@gmail.com>
2023-05-27 18:32:24 -0300

    testando mais reviews

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,6 @@
 set -e
 
 
-if [ ${github.event_name} != "pull_request" ]; then
-      echo "Tipo de evento invalido para avaliacao de codigo"
-fi
-
-
 readme=$(cat README.md | wc -l)
 if [ $readme -le 3 ]; then
       echo "Favor criar um readme para o projeto"
@@ -21,6 +16,6 @@ do
       val=$(grep Mapper $f)
       if [ $val != "" ]; then
             echo "existe mapper"
-            gh pr close ${github.event.pull_request._links.self.href} -c "PR fechada pois não atendeu o código não possui Mapper(componentModel = Spring)"
+            gh pr close ${INPUT_PRLINK} -c "PR fechada pois não atendeu o código não possui Mapper(componentModel = Spring)"
       fi
 done
