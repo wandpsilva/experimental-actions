fe28743c02108e79d5a43b737f2f4bc93fe9a0ef
wander <wanderdesigner@gmail.com>
2023-05-27 17:09:31 -0300

    testando mais reviews

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+      
+      public Main() {}
+      
+      @Mapper
+      public void mapper() {
+            System.out.println("this is just for testing!")
+      }
+}
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,20 @@
 set -e
 readme=$(cat README.md | wc -l)
 
-if [ $readme -le 1 ]; then
+if [ $readme -le 3 ]; then
       echo "Favor criar um readme para o projeto"
       exit 1
 fi
+
+mapper="false"
+for f in ./*.java
+do
+      echo $f
+      val=$(grep Mapper $f)
+      if [ $val != "" ]; then
+            echo "existe mapper"
+      fi
+done
+
+
+#gh pr close ${{ github.pulls_url }} -c "PR fechada pois não atendeu o código não possui Mapper(componentModel = Spring)"
