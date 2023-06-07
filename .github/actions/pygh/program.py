148fe9a9504f3467fb41fc3addebb858cebaf76a
wander <wanderdesigner@gmail.com>
2023-06-07 19:59:59 -0300

    debug python

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -1,20 +1,26 @@
 from github import Github
 
-# First create a Github instance:
+def run():
+    token = os.environ['INPUT_GHTOKEN']
+    print(token)
 
-# using an access token
-g = Github("access_token")
-print('TENTANDO EXIBIR TOKEN')
-print(g)
+    # using an access token
+    g = Github(token)
+    print('TENTANDO EXIBIR TOKEN')
+    print(g)
 
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-print('TENTANDO EXIBIR BASE URL')
-print(g)
+    # Github Enterprise with custom hostname
+    g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+    print('TENTANDO EXIBIR BASE URL')
+    print(g)
 
-repo = g.get_repo("PyGithub/PyGithub")
-print(repo)
+    repo = g.get_repo("PyGithub/PyGithub")
+    print(repo)
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+    # Then play with your Github objects:
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+
+
+if __name__ == '__main__':
+      run()
