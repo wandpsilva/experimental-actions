aa0825c4a6eb2d6b2e46039dfa0cf672cf574d15
wander <wanderdesigner@gmail.com>
2023-06-07 19:45:48 -0300

    debug python

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -4,14 +4,16 @@ from github import Github
 
 # using an access token
 g = Github("access_token")
+print(g)
 
-print("$$$$$$$$$$$$$$$$$$$$$$$ ESTOU AQUI $$$$$$$$$$$$$$$$$$$$$$$")
+# Github Enterprise with custom hostname
+g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+print(g)
 
 repo = g.get_repo("PyGithub/PyGithub")
 print(repo)
 
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+print("$$$$$$$$$$$$$$$$$$$$$$$ ESTOU AQUI $$$$$$$$$$$$$$$$$$$$$$$")
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
