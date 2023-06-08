73e06df65b2438f4433473cd5ea45301bbc3374a
wander <wanderdesigner@gmail.com>
2023-06-08 13:02:10 -0300

    using python

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -1,21 +1,7 @@
-from github import Github
 import os
 
 def run():
-    token = os.environ['token']
-    print(token)
-
-    # using an access token
-    g = Github(token)
-
-    # Github Enterprise with custom hostname
-    g = Github(base_url="https://api.github.com/api/v3", login_or_token="access_token")
-
-    repo = g.get_repo("PyGithub/PyGithub")
-
-    # Then play with your Github objects:
-    for repo in g.get_user().get_repos():
-        print(repo.name)
+    print('Olá mundo!')
 
 
 if __name__ == '__main__':
