3fd5388a63f4f3470e296d5782e94a8a7e047c9b
wander <wanderdesigner@gmail.com>
2023-06-08 14:18:21 -0300

    ajuste variavel

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -4,7 +4,7 @@ def run():
     with open('Main.java') as f:
         lines = f.readlines()
 
-    print('arquivo lido: ${lines}')
+    print(f'arquivo lido: ${lines}')
 
 
 if __name__ == '__main__':
