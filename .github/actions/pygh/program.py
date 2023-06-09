42c681354a8e214c5e286bc48c96dcced7752646
wander <wanderdesigner@gmail.com>
2023-06-09 19:18:23 -0300

    acessando api do github

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -1,10 +1,37 @@
 import os
+import requests
+import sys
+import json
 
 def run():
+
+    try:
+        call_gh_api()
+        read_files()
+    except Exception as ex:
+        print(f'ERRO: {ex}')
+
+
+def read_files():
     with open('Main.java') as f:
         lines = f.readlines()
 
     print(f'arquivo lido: ${lines}')
+
+
+def call_gh_api():
+    url = "https://api.github.com/search/repositories?"
+    querystring = {"q":"wandpsilva"}
+    headers = {
+        'Accept': "application/vnd.github+json",
+        'X-GitHub-Api-Version': "2022-11-28",
+        'Authorization': "Bearer "
+    }
+    data = requests.get(url, headers=headers, params=querystring, verify=False)
+    response = json.loads(data.content)
+
+    repo_name = response['items'][0]['name']
+    print(repo_name)
 
 
 if __name__ == '__main__':
