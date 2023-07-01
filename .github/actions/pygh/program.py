44e1a14c96bae4a1c6947edce4762a3886fc4f59
wander <wanderdesigner@gmail.com>
2023-07-01 17:30:05 -0300

    getting commits

--- .github/actions/pygh/program.py
+++ .github/actions/pygh/program.py
@@ -30,7 +30,7 @@ def call_gh_pygithub(token):
         branch_name = branch.name
         sha = branch.commit.sha
         #commit = g.get_repo(repo).get_commit(sha)
-        print(branch.commit.author.date)
+        print(branch.commit.author)
         print("-----------------------------------------")
         if branch_name == "teste":
             print(f'deleting branch: {branch_name}......')
